43b2d3819bbbf7557efa8bff6c700a4dee34bf1d
dharaswamy <dharaswamy143y@gmail.com>
2025-10-16 18:57:10 +0530

    ahb config agent is instantiated in ahb_env and virtual interface handle assigned

--- ahb_env.sv
+++ ahb_env.sv
@@ -7,6 +7,9 @@ class ahb_env extends uvm_env;
 	//
 	`uvm_component_utils(ahb_env)
 
+	//ahb config agent variable declaration
+	ahb_config_agent ahb_cfg_agt_inst;
+
 	// ahb agent varaible declaration
 	ahb_agent ahb_agt_inst;
 
@@ -16,7 +19,8 @@ class ahb_env extends uvm_env;
 	// ahb subscriber variable declaration
 	ahb_subscriber ahb_sub_inst;
 
-
+        // ahb virtual handle declaration
+	virtual ahb_interface ahb_vintf;
 
 	// Funtion new();
 	//  
@@ -27,6 +31,13 @@ class ahb_env extends uvm_env;
 
 	virtual function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
+		// Creating a object for the ahb config agent 
+	        ahb_cfg_agt_inst=ahb_config_agent::type_id::create("ahb_cfg_agt_inst");
+		// Assigning the virtual interface handle to actouao interface
+		// handle 
+		ahb_cfg_agt_inst.ahb_vintf = this.ahb_vintf;
+		// And setting the ahb config agent handle into config db 
+		uvm_config_db#(ahb_config_agent)::set(this,"ahb_agt_inst*","ahb_cfg_agt_inst",ahb_cfg_agt_inst);
 		// Creating object for the ahb_agent
 		ahb_agt_inst=ahb_agent::type_id::create("ahb_agt_inst",this);
 		//Creating object for the ahb_scoreboard
