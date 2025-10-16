714732f76b8b66618d62a6f88cf679e51db9dae2
dharaswamy <dharaswamy143y@gmail.com>
2025-10-16 17:21:13 +0530

    Implemented analysis port in ahb monitor and created objected for it

--- ahb_monitor.sv
+++ ahb_monitor.sv
@@ -8,13 +8,21 @@ class ahb_monitor extends uvm_monitor;
 
 
 	// Virtaul interface 
-	virtual ahb_interface ahb_vintf; 
+	virtual ahb_interface ahb_vintf;
 
+	// analysis port 
+	uvm_analysis_port#(ahb_sequ_item_base) a_prt ; 
+
+	// 
+	ahb_sequ_item_base ahb_sequ_item; 
 	
 	// new() 
 	//
 	function new(string name=get_type_name(),uvm_component parent);
 		super.new(name,parent);
+		`uvm_info("TRACE",$sformatf("%m"),UVM_HIGH)
+		// Creating object for the analysis port 
+		a_prt= new("a_prt",this);
 	endfunction:new
 
 	//build_phase 
@@ -28,6 +36,16 @@ class ahb_monitor extends uvm_monitor;
 	//
 	virtual protected task run_phase(uvm_phase phase);
 		super.run_phase(phase);
+		wait(ahb_vintf.hresetn);
+		forever begin
+ 			// creating object 
+			ahb_sequ_item=ahb_sequ_item_base::type_id::create("ahb_sequ_item");
+			// Callin the sample task 
+
+			// Broadcasting the transtions using write method.
+			a_prt.write(ahb_sequ_item);
+
+		end
 
 	endtask:run_phase
 
