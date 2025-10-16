8cf8f7cbce193af1d3b3eff6304fccec416321c1
dharaswamy <dharaswamy143y@gmail.com>
2025-10-16 17:00:57 +0530

    Added clocking blocks and modports for the ahb_driver and ahb_monitor

--- ahb_interface.sv
+++ ahb_interface.sv
@@ -22,6 +22,56 @@ interface ahb_interface(input hclk,hresetn);
 
 
 
+	clocking ahb_drv_cb@(posedge hclk);
+		default input #1 output #1 ; 
+		
+		input hrdata;
+		input hready;
+		input hresp;
+		input hexokay;
+
+		output hselx ;
+		output haddr ; 
+		output hburst;
+		output hmastlock;
+		output hprot;
+		output hsize;
+		output hnonsec;
+		output hexcl;
+		output hmaster;
+		output htrans;
+		output hwdata;
+		output hwrite;
+
+	endclocking:ahb_drv_cb
+
+	modport ahb_drv_mp(clocking ahb_drv_cb);
+
+	clocking ahb_mntr_cb@(posedge hclk);
+		default input #1 output #1  ; 
+
+		input hselx ;
+		input haddr ;
+		input hburst	;
+		input hmastlock	;
+		input hprot	;
+		input hsize	;
+		input hnonsec	;
+		input hexcl	;
+		input hmaster	;
+		input htrans	;
+		input hwdata	;
+		input hwrite	;
+		input hrdata	;
+		input hready	;
+		input hresp	;
+		input hexokay	;
+		
+	endclocking:ahb_mntr_cb
+	
+	modport ahb_mntr_mp(clocking ahb_mntr_cb);
+
+
 endinterface:ahb_interface
 
 
